efdbac592e3dd1f6a0691b6fb5f65306212a816c
Anil Kumar <anil.akbp@gmail.com>
2015-11-03 03:53:40 -0800

    Final Changes

--- index.php
+++ index.php
@@ -6,24 +6,37 @@ use RingCentral\SDK;
 
 require('vendor/autoload.php');
 
-// require(__DIR__ . '/demo/callRecording.php');
+
 
 if (!$argv || !in_array('skipSMS', $argv)) {
-	print "Test 4: sms.php\n";
+	print "Test 1: sms.php\n";
     require(__DIR__ . '/demo/sms.php');
 } else {
-	print "Test 4: sms.php - skipping...\n";
+	print "Test 1: sms.php - skipping...\n";
+}
+
+if (!$argv || !in_array('skipCallLog', $argv)) {
+	print "Test 2: call_log.php\n";
+    require(__DIR__ . '/demo/call_log.php');
+} else {
+	print "Test 2: call_log.php - skipping...\n";
 }
 
 if (!$argv || !in_array('skipRingOut', $argv)) {
-	print "Test 5: ringout.php\n";
+	print "Test 3: ringout.php\n";
 	print "Ring-Out Initiated\n";
     require(__DIR__ . '/demo/ringout.php');
 } else {
-	print "Test 5: ringout.php - skipping...\n";
+	print "Test 3: ringout.php - skipping...\n";
 }
 
-require(__DIR__ . '/demo/callRecording_S3.php');
+if (!$argv || !in_array('skipDownloadS3', $argv)) {
+	print "Test 4: callRecording_S3.php\n";
+	print "Ring-Out Initiated\n";
+    require(__DIR__ . '/demo/callRecording_S3.php.php');
+} else {
+	print "Test 4: callRecording_S3.php.php - skipping...\n";
+}
 
-// sleep(30);
+
 
