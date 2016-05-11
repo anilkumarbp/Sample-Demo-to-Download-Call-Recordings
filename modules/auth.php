bbd660debe249b1fa857b14487cf5d2ba4fe9879
Colin Liu <colin198702@gmail.com>
2016-05-11 22:31:09 +0800

    Bug fixing for auth module

--- modules/auth.php
+++ modules/auth.php
@@ -10,31 +10,26 @@ try {
         mkdir($cacheDir);
     }
 
-    $cachedAuth = array();
-
-    function authorize($platform, $cachedAuthData, $file) {
-        $auth = $platform->login($_ENV['RC_Username'], $_ENV['RC_Extension'], $_ENV['RC_Password']);
-        $platform->auth()->setData($cachedAuthData);
-        file_put_contents($file, json_encode($platform->auth()->data(), JSON_PRETTY_PRINT));
+    function authorize($platform, $file) {
     }
     
     if (file_exists($file)) {
         $cachedAuth = json_decode(file_get_contents($file), true);
-        unlink($file); // dispose cache file, it will be updated if script ends successfully
         $platform->auth()->setData($cachedAuth);
         
         if(!$platform->loggedIn()) {
-            authorize($platform, $cachedAuth, $file);
+            $platform->login($_ENV['RC_Username'], $_ENV['RC_Extension'], $_ENV['RC_Password']);
         }
     }else {
-        authorize($platform, $cachedAuth, $file);
+        $platform->login($_ENV['RC_Username'], $_ENV['RC_Extension'], $_ENV['RC_Password']);
     }
+    file_put_contents($file, json_encode($platform->auth()->data(), JSON_PRETTY_PRINT));
+    
     print 'Authorization was restored' . PHP_EOL;
 
 } catch (Exception $e) {
 
     print 'Auth exception: ' . $e->getMessage() . PHP_EOL;
-    print 'Authorized' . PHP_EOL;
     throw $e;    
 }	
 
